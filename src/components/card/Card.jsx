5325c86644d0ccb8fd283fdd9ad49b2372a79d03
Carlitossaul <carlitossaul.h@gmail.com>
2023-04-08 13:25:07 -0300

    s

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,8 +5,18 @@ const Card = ({ image, alt, name, diets }) => {
   return (
     <div className={style.card}>
       <img className={style.img} src={image} alt={alt} />
-      <h1>{name}</h1>
-      {Array.isArray(diets) && diets.map((dieta) => <p>{dieta}</p>)}
+      <h1 className={style.title}>{name}</h1>
+      <span className={style.span}>
+        {" "}
+        Diets:{" "}
+        {Array.isArray(diets) &&
+          diets.map((dieta) => (
+            <ul className={style.ul}>
+              <li className={style.li}>{dieta}</li>
+            </ul>
+          ))}
+      </span>
+      <button className={style.button}>Directions</button>
     </div>
   );
 };
