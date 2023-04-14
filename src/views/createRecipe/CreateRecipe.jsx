59b36087167b78c40de2c68776a142562e3d86a5
Carlitossaul <carlitossaul.h@gmail.com>
2023-04-13 22:28:54 -0300

    s

--- src/views/createRecipe/CreateRecipe.jsx
+++ src/views/createRecipe/CreateRecipe.jsx
@@ -7,18 +7,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { getDiets } from "../../redux/actions";
 
 const CreateRecipe = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getDiets());
   }, []);
   let diets = useSelector((state) => state.diets);
-  const navigate = useNavigate();
+
   const [inputs, setInputs] = useState({
     name: "",
     summary: "",
-    healthScore: 0,
-    servings: 0,
-    readyInMinutes: 0,
+    healthScore: "",
+    servings: "",
+    readyInMinutes: "",
     steps: "",
     image: "",
     diets: [],
@@ -27,9 +28,9 @@ const CreateRecipe = () => {
     name: "",
     summary: "",
     healthScore: "",
-    servings: 0,
-    readyInMinutes: 0,
-    steps: 0,
+    servings: "",
+    readyInMinutes: "",
+    steps: "",
     image: "",
     diets: [],
   });
@@ -39,32 +40,17 @@ const CreateRecipe = () => {
       ...inputs,
       [e.target.name]: e.target.value,
     });
-    // setErrors(
-    //   validation({
-    //     ...inputs,
-    //     [e.target.name]: e.target.value,
-    //   })
-    // );
+    setErrors(
+      validation({
+        ...inputs,
+        [e.target.name]: e.target.value,
+      })
+    );
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // if (inputs.name.length > 30 || inputs.pais.length > 30) {
-    //   errors.name = "Nombre u Pais demasiado largo";
-    // } else if (inputs.numeroCamiseta < 0) {
-    //   errors.numeroCamiseta = "El numero de camiseta tiene que ser mayor a 0";
-    // } else {
-    // }
     axios.post("http://localhost:3001/recipes", inputs);
-    // {
-    //   name: inputs.name,
-    //   image: inputs.image,
-    //   summary: inputs.summary,
-    //   healthScore: inputs.healthScore,
-    //   steps: inputs.steps,
-    //   diets: inputs.diets,
-    // }
-    console.log(inputs);
   };
 
   return (
@@ -73,81 +59,135 @@ const CreateRecipe = () => {
         onClick={() => {
           navigate("/home");
         }}
+        className={style.button}
       >
         Go Back!
       </button>
       <form className={style.form} onSubmit={handleSubmit}>
-        <label htmlFor="name">Name: </label>
-        <input
-          type="text"
-          name="name"
-          onChange={handleChange}
-          value={inputs.name}
-        />
-        <span className={style.span}>{errors.name ? errors.name : ""}</span>
+        <h1 className={style.title}>Creating recipe: {inputs.name} </h1>
+        <div className={style.containerForm}>
+          <div>
+            <label className={style.label} htmlFor="name">
+              Name:
+            </label>
+            <input
+              type="text"
+              name="name"
+              onChange={handleChange}
+              value={inputs.name}
+            />
+            <span className={style.span}>
+              {errors.name ? errors.name : " "}
+            </span>
 
-        <label htmlFor="summary">Summary: </label>
-        <input
-          type="text"
-          name="summary"
-          onChange={handleChange}
-          value={inputs.summary}
-        />
-        {errors.summary && <p>{errors.summary}</p>}
-        <label htmlFor="healthScore">Health Score: </label>
-        <input
-          type="number"
-          name="healthScore"
-          onChange={handleChange}
-          value={inputs.healthScore}
-        />
-        {errors.healthScore && <p>{errors.healthScore}</p>}
-        <label htmlFor="readyInMinutes">readyInMinutes: </label>
-        <input
-          type="number"
-          name="readyInMinutes"
-          onChange={handleChange}
-          value={inputs.readyInMinutes}
-        />
-        {errors.readyInMinutes && <p>{errors.readyInMinutes}</p>}
-        <label htmlFor="servings">servings: </label>
-        <input
-          type="number"
-          name="servings"
-          onChange={handleChange}
-          value={inputs.servings}
-        />
-        {errors.servings && <p>{errors.servings}</p>}
+            <label className={style.label} htmlFor="servings">
+              Servings:
+            </label>
+            <input
+              type="number"
+              name="servings"
+              onChange={handleChange}
+              value={inputs.servings}
+            />
+            <span className={style.span}>
+              {errors.servings ? errors.servings : " "}
+            </span>
 
-        <label htmlFor="steps">Steps: </label>
-        <input
-          type="text"
-          name="steps"
-          onChange={handleChange}
-          value={inputs.steps}
-        />
-        <label htmlFor="image">Image: </label>
-        <input
-          type="text"
-          name="image"
-          onChange={handleChange}
-          value={inputs.image}
-        />
-        <label htmlFor="diets">Diets: </label>
-        <select
-          name="diets"
-          id="diets"
-          onChange={(e) =>
-            setInputs({ ...inputs, diets: [...inputs.diets, e.target.value] })
-          }
-        >
-          {diets.length &&
-            diets.map((diet) => {
-              return <option value={diet}>{diet}</option>;
-            })}
-        </select>
+            <label className={style.label} htmlFor="healthScore">
+              Health Score:{" "}
+            </label>
+            <input
+              type="number"
+              name="healthScore"
+              onChange={handleChange}
+              value={inputs.healthScore}
+            />
+            <span className={style.span}>
+              {errors.name ? errors.name : " "}
+            </span>
 
-        <button type="submit">Save Recipe</button>
+            <label className={style.label} htmlFor="readyInMinutes">
+              readyInMinutes:{" "}
+            </label>
+            <input
+              type="number"
+              name="readyInMinutes"
+              onChange={handleChange}
+              value={inputs.readyInMinutes}
+            />
+
+            <span className={style.span}>
+              {errors.name ? errors.name : " "}
+            </span>
+            <label className={style.label} htmlFor="image">
+              Image:{" "}
+            </label>
+            <input
+              type="text"
+              name="image"
+              onChange={handleChange}
+              value={inputs.image}
+            />
+            <span className={style.span}>
+              {errors.image ? errors.image : " "}
+            </span>
+            <label className={style.label} htmlFor="diets">
+              Diets:{" "}
+            </label>
+            <select
+              name="diets"
+              id="diets"
+              onChange={(e) =>
+                setInputs({
+                  ...inputs,
+                  diets: [...inputs.diets, e.target.value],
+                })
+              }
+            >
+              {diets.length &&
+                diets.map((diet) => {
+                  return <option value={diet}>{diet}</option>;
+                })}
+            </select>
+          </div>
+
+          <div>
+            <div className={style.containerTextArea}>
+              <label className={style.label} htmlFor="summary">
+                Summary:{" "}
+              </label>
+              <textarea
+                type="text"
+                name="summary"
+                onChange={handleChange}
+                value={inputs.summary}
+              />
+              <span className={style.span}>
+                {errors.name ? errors.name : " "}
+              </span>
+
+              <label className={style.label} htmlFor="steps">
+                Steps:{" "}
+              </label>
+              <textarea
+                type="text"
+                name="steps"
+                onChange={handleChange}
+                value={inputs.steps}
+              />
+              <span className={style.span}>
+                {errors.name ? errors.name : " "}
+              </span>
+            </div>
+            <div>
+              <h1>image </h1>
+              <img className={style.image} src={inputs.image} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className={style.divImage}>
+          <button type="submit">Save Recipe</button>
+        </div>
       </form>
     </div>
   );
--- src/views/createRecipe/validation.js
+++ src/views/createRecipe/validation.js
@@ -1,26 +1,31 @@
-// const validation = ({ name, pais, numeroCamiseta }) => {
-//   const errors = {};
-//   if (name.length > 30) errors.name = "Nombre u Pais demasiado largo";
-//   if (pais.length > 30) errors.pais = "Nombre u Pais demasiado largo";
-//   if (numeroCamiseta < 0)
-//     errors.numeroCamiseta = "El numero de camiseta tiene que ser mayor a 0";
-//   return errors;
-const validation = (inputs) => {
-  let errors = {};
+const validation = ({
+  name,
+  servings,
+  readyInMinutes,
+  healthScore,
+  image,
+  summary,
+  steps,
+  diets,
+}) => {
+  const errors = {};
 
-  if (inputs.name && inputs.name.length > 30) {
-    errors.name = "Name is too long";
-  }
-
-  if (inputs.summary && inputs.summary.length > 100) {
-    errors.summary = "Summary is too long";
-  }
-
-  // ...verificar otras propiedades
+  if (!name) errors.name = "The recipe must have a name!";
+  if (!summary) errors.summary = "Here is a summary of the recipe.";
+  if (!servings)
+    errors.servings = "Please indicate how many servings this recipe yields";
+  if (!readyInMinutes)
+    errors.readyInMinutes = "how long is the preparation time?";
+  if (healthScore > 100 || healthScore <= 0)
+    errors.healthScore =
+      "Indicate with a number from 1 to 100 how healthy this recipe is..";
+  if (!image) errors.image = "Please add the url of the image of your recipe.";
+  if (!steps) errors.steps = "What are the steps to prepare the recipe?";
+  if (!diets)
+    errors.diets =
+      "What type of diet does this recipe belong to? it can be several...";
 
   return errors;
 };
-
-// };
 
 export default validation;
